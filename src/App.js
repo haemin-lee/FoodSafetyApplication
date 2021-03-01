afb17933601f3c786fe4e348f41dce52f8e764bc
Jenna Lau <jennanla@usc.edu>
2021-02-28 18:41:23 -1000

    fixed directory organization, .gitignore for node modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          HELLO this is our baby app
+          HELLO welcome to da baby app
         </p>
       </header>
     </div>
